46a84bf38b7f31f72986e7e95df9bffed98b6af9
Hetore C. Vaccari <hetorevaccari@gmail.com>
2023-03-23 21:07:47 -0300

    Desafio Concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,17 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero  { get; set; }
+        private string Modelo { get; set; }
+        private string Imei   { get; set; }
+        private int Memoria   { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero  = numero;
+            Modelo  = modelo;
+            Imei    = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -22,5 +26,6 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void NovoNumero(string numero);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "11111-1111", modelo: "Nokia 1", imei: "333333333", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.NovoNumero("99999-9999");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero: "22222-2222", modelo: "IPhone 1", imei: "444444444", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+iphone.NovoNumero("88888-8888");
+
+Console.WriteLine("");
